c44921c9f700a294bbeb0924b56ffea45f5c5058
Yaathvik2020 <chinnayaathvik@gmail.com>
2025-11-11 23:37:30 +0530

    Update Record.jsx

--- mern/frontend/src/components/Record.jsx
+++ mern/frontend/src/components/Record.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { useParams, useNavigate } from "react-router-dom";
-const backendUrl = import.meta.env.VITE_BACKEND_URL
+const backendUrl = "http://backend:5050"
 export default function Record() {
   const [form, setForm] = useState({
     name: "",
